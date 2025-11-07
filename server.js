860dff479a53ddf658c564480320b303b791ab01
Rakshu-123 <rakshuvg06@gmail.com>
2025-11-07 23:16:50 +0530

    changes added

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 // Middleware
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
-    ? ['https://vercel-f-blue.vercel.app'] 
+    ? ['https://vercel-b-ten.vercel.app'] 
     : ['http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
